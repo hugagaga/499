33aa76ee39617f340ffe9af1be9a9adfabcfe700
hugagaga <joehu1996@gmail.com>
2020-02-03 17:23:56 -0800

    1. Replace std::cout with glog 2. use specific return value to hangle error message 3. update unit tests

--- src/kvstore/kvmap.cc
+++ src/kvstore/kvmap.cc
@@ -6,24 +6,26 @@
 
 Kvmap::Kvmap() = default;
 
-Kvmap::Kvmap(std::vector<std::pair<std::string, std::string>> list) {
+Kvmap::Kvmap(const std::vector<std::pair<std::string, std::string>>& list) {
   for (const auto& p : list) {
     kvmap_.insert(p);
   }
 }
 
-bool Kvmap::put(const std::string& key, const std::string& value) {
+bool Kvmap::Put(const std::string& key, const std::string& value) {
   const auto [it, success] = kvmap_.insert({key, value});
   return success;
 }
 
-std::string Kvmap::get(const std::string& key) {
+std::pair<std::string, int> Kvmap::Get(const std::string& key) {
   bool found = kvmap_.find(key) != kvmap_.end();
-  if (found) return kvmap_[key];
-  return "NOT_FOUND";
+  if (!found) {
+    return {"", -1};
+  }
+  return {kvmap_[key], 0};
 }
 
-bool Kvmap::remove(const std::string& key) {
+bool Kvmap::Remove(const std::string& key) {
   int success = kvmap_.erase(key);
   return success;
 }
--- src/kvstore/kvmap.h
+++ src/kvstore/kvmap.h
@@ -6,13 +6,22 @@
 #include <map>
 
 
+//A class for backend key-value store
 class Kvmap {
  public:
+  //Default Constructor
   Kvmap();
-  Kvmap(std::vector<std::pair<std::string, std::string>>);
-  bool put(const std::string& key, const std::string& value);
-  std::string get(const std::string& key);
-  bool remove(const std::string& key);
+  //Constructor of the kvmap_ from a vector of pairs<string, string>
+  Kvmap(const std::vector<std::pair<std::string, std::string>>&);
+  //Inserts <string, string> pair into the map
+  bool Put(const std::string& key, const std::string& value);
+  //Returns a previously stored value or values under that key and true indicatin
+  //or nothing if the key is not reprent in the store
+  //return status -1: not found, 0 success
+  std::pair<std::string, int> Get(const std::string& key);
+  //deletes all previously stored values under that key
+  bool Remove(const std::string& key);
+
  private:
   std::map<std::string, std::string> kvmap_;
 };
