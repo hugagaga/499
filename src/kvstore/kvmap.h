944b5e66db115422400f32529ce1e6bac0dc99ff
hugagaga <joehu1996@gmail.com>
2020-02-07 13:29:54 -0800

    1. Add header files of client and server source files. 2. Stuck at getting multiple values to one key on GRPC. (One request message for multiple replies)

--- src/kvstore/kvmap.h
+++ src/kvstore/kvmap.h
@@ -3,7 +3,7 @@
 
 #include <vector>
 #include <string>
-#include <map>
+#include <unordered_map>
 #include <optional>
 
 
@@ -15,17 +15,17 @@ class Kvmap {
   //Constructor of the kvmap_ from a vector of pairs<string, string>
   Kvmap(const std::vector<std::pair<std::string, std::string>>&);
   //Inserts <string, string> pair into the map
-  bool Put(const std::string& key, const std::string& value);
+  void Put(const std::string& key, const std::string& value);
   //Returns an optional contained value which is 
   //present when there exits previously stored value or values under that key 
   //not present when if the key is not reprent in the store
   //has_value could be called to check if the returned object contains a value
-  std::optional<std::string> Get(const std::string& key);
+  std::optional<std::vector<std::string>> Get(const std::string& key);
   //deletes all previously stored values under that key
-  bool Remove(const std::string& key);
+  int Remove(const std::string& key);
 
  private:
-  std::map<std::string, std::string> kvmap_;
+  std::unordered_multimap<std::string, std::string> kvmap_;
 };
 
 #endif  // WARBLE_KVSTORE_H_
